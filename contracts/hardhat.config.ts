202b07445e633d1b6a9f166d2af633be2ed7219d
marcuspang <marcuspangyuyang@gmail.com>
2023-09-02 22:49:18 +0200

    feat: update deploy script + add address

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,9 +1,29 @@
 import { HardhatUserConfig } from "hardhat/config";
-require('@openzeppelin/hardhat-upgrades');
+import "@openzeppelin/hardhat-upgrades";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ethers";
+
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    goerli: {
+      url: "https://ethereum-goerli.publicnode.com",
+      accounts: [process.env.SECRET!],
+      chainId: 5,
+    },
+    alfajores: {
+      url: "https://alfajores-forno.celo-testnet.org",
+      accounts: [process.env.SECRET!],
+      chainId: 44787,
+    },
+    mantleTestnet: {
+      url: "https://rpc.testnet.mantle.xyz",
+      accounts: [process.env.SECRET!],
+      chainId: 5001,
+    },
+  },
 };
 
 export default config;
---
+++ frontend/src/constants/address.ts
@@ -0,0 +1,6 @@
+import { celoAlfajores, mantleTestnet } from "wagmi/chains";
+
+export const CONTRACT_ADDRESS = {
+  [celoAlfajores.id]: "0x31933694Ee18C19E69434134642A18C0644905fd",
+  [mantleTestnet.id]: "0x31933694Ee18C19E69434134642A18C0644905fd",
+};
