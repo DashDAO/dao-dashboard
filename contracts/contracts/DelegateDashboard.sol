93d4ae3e9f921d00c78b272e2a9a4289fde853d7
cleanerzkp <104455005+cleanerzkp@users.noreply.github.com>
2023-09-02 17:53:22 +0200

    final

--- contracts/contracts/DelegateDashboard.sol
+++ contracts/contracts/DelegateDashboard.sol
@@ -1,21 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-contract DelegateDashboard {
+contract DelegateFollow {
     address public owner;
 
-    struct Delegate {
-        mapping(address => bool) followers;
-        uint256 lastVotedTimestamp;
-    }
-
-    mapping(address => mapping(address => Delegate)) public daoDelegates;
+    mapping(address => mapping(address => bool)) private followers;
 
     // Events
-    event Followed(address indexed follower, address indexed daoAddress, address indexed delegateAddress);
-    event Unfollowed(address indexed follower, address indexed daoAddress, address indexed delegateAddress);
-    event DelegateVoted(address indexed delegateAddress, address indexed daoAddress);
-    event NotVotedWithinDeadline(address indexed delegateAddress, address indexed daoAddress);
+    event Followed(address indexed follower, address indexed delegate);
+    event Unfollowed(address indexed follower, address indexed delegate);
 
     constructor() {
         owner = msg.sender;
@@ -26,21 +19,25 @@ contract DelegateDashboard {
         _;
     }
 
-    function follow(address daoAddress, address delegateAddress) external {
-        Delegate storage delegateData = daoDelegates[daoAddress][delegateAddress];
-        require(!delegateData.followers[msg.sender], "Already following");
-        delegateData.followers[msg.sender] = true;
-        emit Followed(msg.sender, daoAddress, delegateAddress);
+    function follow(address delegateAddress) external {
+        require(delegateAddress != address(0), "Invalid delegate address");
+        require(delegateAddress != msg.sender, "Cannot follow yourself");
+        require(!followers[msg.sender][delegateAddress], "Already following");
+
+        followers[msg.sender][delegateAddress] = true;
+        emit Followed(msg.sender, delegateAddress);
     }
 
-    function unfollow(address daoAddress, address delegateAddress) external {
-        Delegate storage delegateData = daoDelegates[daoAddress][delegateAddress];
-        require(delegateData.followers[msg.sender], "Not following");
-        delete delegateData.followers[msg.sender];
-        emit Unfollowed(msg.sender, daoAddress, delegateAddress);
+    function unfollow(address delegateAddress) external {
+        require(delegateAddress != address(0), "Invalid delegate address");
+        require(delegateAddress != msg.sender, "Cannot unfollow yourself");
+        require(followers[msg.sender][delegateAddress], "Not following");
+
+        followers[msg.sender][delegateAddress] = false;
+        emit Unfollowed(msg.sender, delegateAddress);
     }
-    
-    function isFollowing(address daoAddress, address delegateAddress, address follower) external view returns (bool) {
-        return daoDelegates[daoAddress][delegateAddress].followers[follower];
+
+    function isFollowing(address follower, address delegateAddress) external view returns (bool) {
+        return followers[follower][delegateAddress];
     }
 }
--- contracts/test/test.ts
+++ contracts/test/test.ts
@@ -1,44 +1,49 @@
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
 
-describe("DelegateDashboard", function () {
-  let owner, daoAddress, delegate, follower1, follower2, dashboard;
+describe("DelegateFollow", function () {
+  let owner, delegate, follower1, follower2, dashboard;
 
   beforeEach(async function () {
     // Deploy the contract
-    const DelegateDashboard = await ethers.getContractFactory("DelegateDashboard");
-    dashboard = await DelegateDashboard.deploy();
+    const DelegateFollow = await ethers.getContractFactory("DelegateFollow");
+    dashboard = await DelegateFollow.deploy();
 
     // Get accounts
-    [owner, daoAddress, delegate, follower1, follower2] = await ethers.getSigners();
+    [owner, delegate, follower1, follower2] = await ethers.getSigners();
   });
 
   describe("Follow & Unfollow", function () {
 
     it("should allow a user to follow a delegate", async function () {
-      await dashboard.connect(follower1).follow(daoAddress.address, delegate.address);
-      const isFollower1Following = await dashboard.isFollowing(daoAddress.address, delegate.address, follower1.address);
+      await dashboard.connect(follower1).follow(delegate.address);
+      const isFollower1Following = await dashboard.isFollowing(follower1.address, delegate.address);
       expect(isFollower1Following).to.be.true;
     });
 
     it("should emit a Followed event when a user follows a delegate", async function () {
-      await expect(dashboard.connect(follower1).follow(daoAddress.address, delegate.address))
+      await expect(dashboard.connect(follower1).follow(delegate.address))
         .to.emit(dashboard, "Followed")
-        .withArgs(follower1.address, daoAddress.address, delegate.address);
+        .withArgs(follower1.address, delegate.address);
     });
 
     it("should allow a user to unfollow a delegate", async function () {
-      await dashboard.connect(follower1).follow(daoAddress.address, delegate.address);
-      await dashboard.connect(follower1).unfollow(daoAddress.address, delegate.address);
-      const isFollower1Following = await dashboard.isFollowing(daoAddress.address, delegate.address, follower1.address);
+      await dashboard.connect(follower1).follow(delegate.address);
+      await dashboard.connect(follower1).unfollow(delegate.address);
+      const isFollower1Following = await dashboard.isFollowing(follower1.address, delegate.address);
       expect(isFollower1Following).to.be.false;
     });
 
     it("should emit an Unfollowed event when a user unfollows a delegate", async function () {
-      await dashboard.connect(follower1).follow(daoAddress.address, delegate.address);
-      await expect(dashboard.connect(follower1).unfollow(daoAddress.address, delegate.address))
+      await dashboard.connect(follower1).follow(delegate.address);
+      await expect(dashboard.connect(follower1).unfollow(delegate.address))
         .to.emit(dashboard, "Unfollowed")
-        .withArgs(follower1.address, daoAddress.address, delegate.address);
+        .withArgs(follower1.address, delegate.address);
+    });
+
+    it("should not allow a user to unfollow if not following", async function () {
+      await expect(dashboard.connect(follower1).unfollow(delegate.address))
+        .to.be.revertedWith("Not following");
     });
   });
 });
