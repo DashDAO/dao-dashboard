0e52b78d8d7237a47f5da7fcca96dd7b98b4a49e
bertankofon <64563825+bertankofon@users.noreply.github.com>
2023-09-02 13:27:31 +0200

    committest

--- contracts/contracts/DelegateFollow.sol
+++ contracts/contracts/DelegateFollow.sol
@@ -1,56 +1,34 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.0;
 
 contract DelegateFollow {
-    
-    address public owner;
-
-    // Struct for Delegate
+ainId) external onlySupportedChain(chainId) {
+        Delegate storage delegateData = daoDelegates[daoAddress][delegateAddress];
+        require(!delegateData.followers[msg.sender], "Already following");
     struct Delegate {
         bool isDelegate;
         uint256 followersCount;
         mapping(address => bool) followers;
     }
 
-    // Mapping for DAO address -> Delegate address -> Delegate details
-    mapping(address => mapping(address => Delegate)) public daoDelegates;  
+    mapping(address => mapping(address => Delegate)) public daoDelegates;  // DAO address -> Delegate address -> Delegate details
     mapping(uint256 => bool) public supportedChains;
 
-    // Events
     event Followed(address indexed follower, address indexed daoAddress, address indexed delegate);
     event Unfollowed(address indexed follower, address indexed daoAddress, address indexed delegate);
 
-  
     constructor() {
-        owner = msg.sender;  // Set the contract deployer as the owner
-        supportedChains[5001] = true;   // Initialized with mantle testnet
-        supportedChains[44787] = true;  // Initialized with celo testnet
+        // Initializing with chain IDs 5001 and 44787 as supported - mantle and celo testnets, we can add more
+        supportedChains[5001] = true;
+        supportedChains[44787] = true;
     }
 
     modifier onlySupportedChain(uint256 chainId) {
         require(supportedChains[chainId], "Chain not supported");
         _;
     }
-    
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Not authorized");
-        _;
-    }
 
-    // Add new supported chain
-    function addSupportedChain(uint256 chainId) external onlyOwner {
-        supportedChains[chainId] = true;
-    }
-
-    // Remove a supported chain
-    function removeSupportedChain(uint256 chainId) external onlyOwner {
-        delete supportedChains[chainId];
-    }
-
-    // Follow a delegate in a specific DAO on a specific chain
-    function follow(address daoAddress, address delegateAddress, uint256 chainId) external onlySupportedChain(chainId) {
-        Delegate storage delegateData = daoDelegates[daoAddress][delegateAddress];
-        require(!delegateData.followers[msg.sender], "Already following");
+    function follow(address daoAddress, address delegateAddress, uint256 ch
 
         if (!delegateData.isDelegate) {
             delegateData.isDelegate = true;
@@ -61,15 +39,16 @@ contract DelegateFollow {
 
         emit Followed(msg.sender, daoAddress, delegateAddress);
     }
-
-    // Unfollow a delegate in a specific DAO on a specific chain
+    
     function unfollow(address daoAddress, address delegateAddress, uint256 chainId) external onlySupportedChain(chainId) {
         Delegate storage delegateData = daoDelegates[daoAddress][delegateAddress];
         require(delegateData.followers[msg.sender], "Not following");
-        
-        delete delegateData.followers[msg.sender];
+
+        delegateData.followers[msg.sender] = false;
         delegateData.followersCount--;
 
         emit Unfollowed(msg.sender, daoAddress, delegateAddress);
     }
 }
+
+//committest to contribute
