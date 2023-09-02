e6385c0c6f8640582973c40fd40a9ecf6ce00a53
marcuspang <marcuspangyuyang@gmail.com>
2023-09-02 14:04:44 +0200

    feat: update proposals endpoint with query params

--- frontend/src/pages/api/proposals.ts
+++ frontend/src/pages/api/proposals.ts
@@ -5,8 +5,16 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const query = `query Proposals {
-    proposals(first: 20, skip: 0, where: {space_in: ["balancer.eth", "yam.eth"], state: "closed"}, orderBy: "created", orderDirection: desc) {
+  const query = `query Proposals($first: Int, $skip: Int, $space: String) {
+    proposals(
+      first: $first,
+      skip: $skip,
+      where: {
+        space: $space
+      }, 
+      orderBy: "created", 
+      orderDirection: desc
+    ) {
       id
       title
       body
@@ -24,11 +32,17 @@ export default async function handler(
   }`;
 
   if (req.method === "GET") {
+    const { space, first, skip } = req.query;
+    if (!space) {
+      throw new Error("No space found");
+    }
     const response = await fetch(SNAPSHOT_API_URL, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
+        operationName: "Proposals",
         query,
+        variables: { first: first || 20, skip: skip || 0, space },
       }),
     });
 
@@ -38,6 +52,4 @@ export default async function handler(
     const data = await response.json();
     return res.status(200).json(data);
   }
-
-  return res.status(404).send("Not found");
 }
--- frontend/src/pages/dao/[id]/index.tsx
+++ frontend/src/pages/dao/[id]/index.tsx
@@ -1,14 +1,13 @@
-import { useRouter } from "next/router";
 import { CacheKey } from "@/constants/cache";
-import Link from "next/link";
+import { useRouter } from "next/router";
 import { useQuery } from "wagmi";
 
 export default function DaoPage() {
   const { query } = useRouter();
   const { id } = query;
-  const { data } = useQuery([CacheKey.PROPOSALS], () =>
-    fetch("/api/proposals").then((res) => res.json())
-  );
+  const { data } = useQuery([CacheKey.PROPOSALS], () => {
+    return fetch(`/api/proposals?space=${id}`).then((res) => res.json());
+  });
 
   if (!data) {
     return <p>Loading...</p>;
@@ -21,17 +20,16 @@ export default function DaoPage() {
       <p>DAOs ID {id}</p>
       <h1>Proposals</h1>
       <div>
-
-      {proposals?.slice(0, 3).map((proposal:any) => {
-        console.log({proposal})
-        return (
-          <div key={proposal.id}>
-            <h2>{proposal.title}</h2>
-            <p>{proposal.body}</p>
-            <p>Author: {proposal.author}</p>
-          </div>
-        );
-      })}
+        {proposals?.slice(0, 3).map((proposal: any) => {
+          console.log({ proposal });
+          return (
+            <div key={proposal.id}>
+              <h2>{proposal.title}</h2>
+              <p>{proposal.body}</p>
+              <p>Author: {proposal.author}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
