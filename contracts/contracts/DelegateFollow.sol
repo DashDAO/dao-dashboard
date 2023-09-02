cce7f1d6ff7e0bfeca9209105270776272514ddb
cleanerzkp <104455005+cleanerzkp@users.noreply.github.com>
2023-09-02 12:59:34 +0200

    -Integrated ownership control for supported chains management. - Optimized the unfollow function using the 'delete' keyword. - Enhanced error messages for better debugging and user experience

--- contracts/contracts/DelegateFollow.sol
+++ contracts/contracts/DelegateFollow.sol
@@ -1,31 +1,53 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 contract DelegateFollow {
+    
+    address public owner;
 
+    // Struct for Delegate
     struct Delegate {
         bool isDelegate;
         uint256 followersCount;
         mapping(address => bool) followers;
     }
 
-    mapping(address => mapping(address => Delegate)) public daoDelegates;  // DAO address -> Delegate address -> Delegate details
+    // Mapping for DAO address -> Delegate address -> Delegate details
+    mapping(address => mapping(address => Delegate)) public daoDelegates;  
     mapping(uint256 => bool) public supportedChains;
 
+    // Events
     event Followed(address indexed follower, address indexed daoAddress, address indexed delegate);
     event Unfollowed(address indexed follower, address indexed daoAddress, address indexed delegate);
 
+  
     constructor() {
-        // Initializing with chain IDs 5001 and 44787 as supported - mantle and celo testnets, we can add more
-        supportedChains[5001] = true;
-        supportedChains[44787] = true;
+        owner = msg.sender;  // Set the contract deployer as the owner
+        supportedChains[5001] = true;   // Initialized with mantle testnet
+        supportedChains[44787] = true;  // Initialized with celo testnet
     }
 
     modifier onlySupportedChain(uint256 chainId) {
         require(supportedChains[chainId], "Chain not supported");
         _;
     }
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not authorized");
+        _;
+    }
 
+    // Add new supported chain
+    function addSupportedChain(uint256 chainId) external onlyOwner {
+        supportedChains[chainId] = true;
+    }
+
+    // Remove a supported chain
+    function removeSupportedChain(uint256 chainId) external onlyOwner {
+        delete supportedChains[chainId];
+    }
+
+    // Follow a delegate in a specific DAO on a specific chain
     function follow(address daoAddress, address delegateAddress, uint256 chainId) external onlySupportedChain(chainId) {
         Delegate storage delegateData = daoDelegates[daoAddress][delegateAddress];
         require(!delegateData.followers[msg.sender], "Already following");
@@ -40,11 +62,12 @@ contract DelegateFollow {
         emit Followed(msg.sender, daoAddress, delegateAddress);
     }
 
+    // Unfollow a delegate in a specific DAO on a specific chain
     function unfollow(address daoAddress, address delegateAddress, uint256 chainId) external onlySupportedChain(chainId) {
         Delegate storage delegateData = daoDelegates[daoAddress][delegateAddress];
         require(delegateData.followers[msg.sender], "Not following");
-
-        delegateData.followers[msg.sender] = false;
+        
+        delete delegateData.followers[msg.sender];
         delegateData.followersCount--;
 
         emit Unfollowed(msg.sender, daoAddress, delegateAddress);
