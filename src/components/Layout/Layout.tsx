4f1c0b0b35fd3b16ab10cde1dc6bcec9e1b4ba3a
marcuspang <marcuspangyuyang@gmail.com>
2023-09-02 09:58:51 +0200

    feat: setup layout + navbar

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react";
+import { NavBar } from "./NavBar";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export function Layout({ children }: { children?: ReactNode }) {
+  return (
+    <>
+      <header className="py-2 border-b">
+        <div className="container">
+          <NavBar />
+        </div>
+      </header>
+      <main
+        className={`flex min-h-screen container flex-col items-center justify-between p-24 ${inter.className}`}
+      >
+        {children}
+      </main>
+    </>
+  );
+}
